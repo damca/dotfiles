21bc13cd1dac189d5ca05b6275c9a9949ac00f5b
damca <dmcaffee@berkeley.edu>
2017-03-07 14:04:33 -0800

    Echo date of backup.sh

--- bin/backupdotfiles.sh
+++ bin/backupdotfiles.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 osascript -e "display notification \"Pausing Dropbox.\" with title \"Backing up ~/.dotfiles\""
 dropbox-pause-unpause.sh --pause
-cp -rf ~/.dotfiles ~/Dropbox/dotfiles > ~/.dotfiles/backup.log 2> ~/.dotfiles/backuperr.log
+cp -rf ~/.dotfiles ~/Dropbox/dotfiles 2> ~/.dotfiles/backuperr.log
+echo "Backed up dotfiles $(date +%d.%m.%y-%H:%M:%S)" >> ~/.dotfiles/backup.log
 dropbox-pause-unpause.sh --unpause
 osascript -e "display notification \"Unpausing Dropbox.\" with title \"Finished backing up ~/.dotfiles\""
--- bin/backupgit.sh
+++ bin/backupgit.sh
@@ -3,5 +3,6 @@
 osascript -e "display notification \"Pausing Dropbox.\" with title \"Backing up ~/git\""
 dropbox-pause-unpause.sh --pause
 cp -rf ~/git ~/Dropbox >~/git/backup.log 2>~/git/backuperr.log
+echo "Backed up dotfiles $(date +%d.%m.%y-%H:%M:%S)" >> ~/.dotfiles/backup.log
 dropbox-pause-unpause.sh --unpause
 osascript -e "display notification \"Unpausing Dropbox.\" with title \"Finished backing up ~/git\""
