10f56024a443991cfe49199f0e97a879ae2bb33f
damca <dmcaffee@berkeley.edu>
2020-10-01 11:01:24 -0700

    updated apt and network files

--- install/apt.sh
+++ install/apt.sh
@@ -10,12 +10,12 @@ sudo apt upgrade
 sudo apt install ack tree curl man-db manpages fzf
 
 # development server setup
-sudo apt install nginx dnsmasq dnsutils ftp vsftpd nmap
+sudo apt install nginx dnsmasq dnsutils ftp vsftpd nmap net-tools
 
 # development tools
 sudo apt install git zsh python python3 python3-venv
 
-# get image processing tools, most of these are for the python imagecodecs package
+# get image processing tools, most of these are for the python "imagecodecs" package
 sudo apt-get install build-essential python3-dev cython3 python3-setuptools python3-pip python3-wheel python3-numpy python3-pytest python3-blosc python3-brotli python3-snappy python3-lz4 libz-dev libblosc-dev liblzma-dev liblz4-dev libzstd-dev libpng-dev libwebp-dev libbz2-dev libopenjp2-7-dev libjpeg-dev libjxr-dev liblcms2-dev libcharls-dev libaec-dev libbrotli-dev libsnappy-dev libzopfli-dev libgif-dev libtiff-dev qt5-default
 
 # python environments
